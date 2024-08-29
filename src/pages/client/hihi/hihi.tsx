815eb576743c3896b1799533303acbec9d4dd50c
ducnt28 <nguyentrungduc2807@gmail.com>
2024-08-29 23:37:16 +0700

    first commit

---
+++ src/pages/client/hihi/hihi.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Hihi = (props: Props) => {
+    return (
+        <div>
+            hihii
+        </div>
+    )
+}
+
+export default Hihi
